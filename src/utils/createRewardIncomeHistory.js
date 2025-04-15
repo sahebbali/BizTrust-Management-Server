1fe9e223af580172bf651be3bb41a51fad6da990
sahebbali <saheb1996jmr@gmail.com>
2025-04-15 08:28:03 +0600

    delete some

---
+++ src/utils/createRewardIncomeHistory.js
@@ -0,0 +1,68 @@
+const RewardIncomeModel = require("../models/rewardIncome.model");
+const User = require("../models/auth.model");
+const getPSTime = require("../config/getPSTime");
+const { UpdateWallet } = require("./checkPackageLimit");
+const generateRandomString = require("../config/generateRandomId");
+
+const CreateRewardHistory = async (
+  userId,
+  designation,
+  amount,
+  position,
+  line1,
+  line2,
+  line3,
+  line4,
+  line5
+) => {
+  console.log("reward income :", userId);
+  const { date, time } = getPSTime();
+  const currentUser = await User.findOne({ userId });
+
+  if (!currentUser) {
+    console.log("User not found:", userId);
+    return;
+  }
+
+  // console.log("Current User:", currentUser);
+
+  const existReward = await RewardIncomeModel.findOne({
+    userId,
+    rewardDesignation: designation,
+    rewardPosition: position,
+  });
+
+  if (existReward) {
+    console.log("Reward already exists for:", designation);
+    return;
+  }
+
+  await RewardIncomeModel.create({
+    userId: currentUser.userId,
+    fullName: currentUser.fullName,
+    sponsorId: currentUser.sponsorId,
+    sponsorName: currentUser.sponsorName,
+    rewardDesignation: designation,
+    rewardPosition: position,
+    rewardAmount: typeof amount === "number" ? amount : 0,
+    rewardGift: typeof amount === "string" ? amount : "",
+    line1,
+    line2,
+    line3,
+    line4,
+    line5,
+    date: new Date(date).toDateString(),
+    time,
+    transactionId: generateRandomString(13),
+  });
+  await User.findOneAndUpdate(
+    { userId: currentUser.userId },
+    { $set: { openLevel: 5, rank: designation } },
+    { new: true }
+  );
+  if (typeof amount === "number") {
+    await UpdateWallet(currentUser.userId, amount, "reward-income");
+  }
+};
+
+module.exports = CreateRewardHistory;
